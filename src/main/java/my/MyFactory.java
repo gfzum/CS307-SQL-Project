53e12a97fe3d296faf16230332149666a176578e
jr <1975863521@qq.com>
2021-12-12 21:53:50 +0800

    add my package

---
+++ src/main/java/my/MyFactory.java
@@ -0,0 +1,20 @@
+package my;
+
+import cn.edu.sustech.cs307.factory.ServiceFactory;
+import cn.edu.sustech.cs307.service.CourseService;
+import my.services.MyCourseService;
+
+import java.util.List;
+
+public class MyFactory extends ServiceFactory {
+    public MyFactory() {
+        super();
+        registerService(CourseService.class, new MyCourseService());
+
+    }
+
+    @Override
+    public List<String> getUIDs() {
+        return List.of("12011216", "12011412", "12011411");
+    }
+}
---
+++ src/main/java/my/services/MyDepartmentService.java
@@ -0,0 +1,42 @@
+package my.services;
+
+import cn.edu.sustech.cs307.database.SQLDataSource;
+import cn.edu.sustech.cs307.dto.Department;
+import cn.edu.sustech.cs307.service.DepartmentService;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+public class MyDepartmentService implements DepartmentService{
+    @Override
+    public int addDepartment(String name) {
+
+        try (Connection connection = SQLDataSource.getInstance().getSQLConnection();
+             PreparedStatement stmt = connection.prepareStatement("insert into department(n) values (?)")) {
+//            //stmt.setInt(1, 1);
+//            stmt.setString(, name);
+//            stmt.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return 0;
+    }
+
+    @Override
+    public void removeDepartment(int departmentId) {
+
+    }
+
+    @Override
+    public List<Department> getAllDepartments() {
+        return null;
+    }
+
+    @Override
+    public Department getDepartment(int departmentId) {
+        return null;
+    }
+}
