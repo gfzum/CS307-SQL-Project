8546f4f3c7d8f12ec750ddf400a7521ebcc40f4f
gfzum <w48905833@163.com>
2021-12-13 01:14:08 +0800

    department部分除了add以外写完

--- src/main/java/my/services/MyDepartmentService.java
+++ src/main/java/my/services/MyDepartmentService.java
@@ -6,7 +6,9 @@ import cn.edu.sustech.cs307.service.DepartmentService;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MyDepartmentService implements DepartmentService{
@@ -34,6 +36,7 @@ public class MyDepartmentService implements DepartmentService{
             PreparedStatement stm = connection.prepareStatement(sql);
             stm.setInt(1,departmentId);
             stm.executeUpdate();
+            stm.close();
 
         } catch (SQLException e){
             e.printStackTrace();
@@ -42,11 +45,50 @@ public class MyDepartmentService implements DepartmentService{
 
     @Override
     public List<Department> getAllDepartments() {
+        try (Connection connection = SQLDataSource.getInstance().getSQLConnection()){
+            String sql = "select * from department";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            ResultSet rs = stm.executeQuery();
+
+            List<Department> list = new ArrayList<>();
+            if (rs.next()){
+                Department temp = new Department();
+                temp.id = rs.getInt(1);
+                temp.name = rs.getString(2);
+
+                list.add(temp);
+            }
+            stm.close();
+            rs.close();
+            return list;
+
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+
         return null;
     }
 
     @Override
     public Department getDepartment(int departmentId) {
+
+        try (Connection connection = SQLDataSource.getInstance().getSQLConnection()){
+            String sql = "select * from department where dept_id = ?";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            ResultSet rs = stm.executeQuery();
+
+            Department temp = new Department();
+            if (rs.next()){
+                temp.id = rs.getInt(1);
+                temp.name = rs.getString(2);
+            }
+            stm.close();
+            rs.close();
+            return temp;
+
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
         return null;
     }
 }
