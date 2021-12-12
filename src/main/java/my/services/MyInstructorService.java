1aa1ee1c71b37defd32f46ecf2a85a04fd67e4c8
SkyLYnf <12011412@mail.sustech.edu.cn>
2021-12-12 23:50:57 +0800

    更新了addInstructor

--- src/main/java/my/services/MyInstructorService.java
+++ src/main/java/my/services/MyInstructorService.java
@@ -1,14 +1,27 @@
 package my.services;
 
+import cn.edu.sustech.cs307.database.SQLDataSource;
 import cn.edu.sustech.cs307.dto.CourseSection;
 import cn.edu.sustech.cs307.service.InstructorService;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 public class MyInstructorService implements InstructorService{
     @Override
     public void addInstructor(int userId, String firstName, String lastName) {
-
+        try (Connection connection = SQLDataSource.getInstance().getSQLConnection();
+             PreparedStatement stmt = connection.prepareStatement("insert into instructor(instructor_id, first_name, last_name) " +
+                     "values (?,?,?)")) {
+            stmt.setInt(1,userId);
+            stmt.setString(2,firstName);
+            stmt.setString(3,lastName);
+            stmt.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
