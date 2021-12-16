e29e885fd2ebd4868d9c76d066d2e687715ce10e
jr <1975863521@qq.com>
2021-12-16 23:16:03 +0800

    再同步一下

--- src/main/java/my/services/MyCourseService.java
+++ src/main/java/my/services/MyCourseService.java
@@ -6,6 +6,8 @@ import cn.edu.sustech.cs307.dto.CourseSection;
 import cn.edu.sustech.cs307.dto.CourseSectionClass;
 import cn.edu.sustech.cs307.dto.Student;
 import cn.edu.sustech.cs307.dto.prerequisite.Prerequisite;
+import cn.edu.sustech.cs307.exception.EntityNotFoundException;
+import cn.edu.sustech.cs307.exception.IntegrityViolationException;
 import cn.edu.sustech.cs307.service.CourseService;
 
 import javax.annotation.Nullable;
@@ -14,6 +16,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.DayOfWeek;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -21,53 +24,65 @@ public class MyCourseService implements CourseService {
     @Override
     public void addCourse(String courseId, String courseName, int credit, int classHour, Course.CourseGrading grading, @Nullable Prerequisite prerequisite) {
         try (Connection connection = SQLDataSource.getInstance().getSQLConnection();
-             PreparedStatement stmt = connection.prepareStatement("insert into course values (?,?,?,?,?)")) {
+             PreparedStatement stmt = connection.prepareStatement("insert into course (course_id, course_name, credit, class_hour, grading) values (?,?,?,?,?)")) {
             stmt.setString(1, courseId);
             stmt.setString(2, courseName);
             stmt.setInt(3, credit);
             stmt.setInt(4, classHour);
-            //
-            stmt.execute();
+            String Grade = grading.toString();
+            stmt.setString(5,Grade);
+            stmt.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
+            throw new IntegrityViolationException();
         }
     }
 
     @Override
     public int addCourseSection(String courseId, int semesterId, String sectionName, int totalCapacity) {
-        String sql = "insert into course_section" + "(course_id, semester_id, section_name, total_capacity)" + "values(?, ?, ?, ?);";
-        try (Connection connection = SQLDataSource.getInstance().getSQLConnection();
-             PreparedStatement stmt = connection.prepareStatement(sql,PreparedStatement.RETURN_GENERATED_KEYS)) {
-            int back;
-            stmt.setString(1, courseId);
-            stmt.setInt(2, semesterId);
-            stmt.setString(3, sectionName);
-            stmt.setInt(4, totalCapacity);
-            stmt.executeUpdate();
-            ResultSet resultSet = stmt.getGeneratedKeys();
-            if(resultSet.next()) {
-                back = resultSet.getInt(3);
-                connection.commit();
-                connection.close();
-                return back;
+        Connection connection = null;
+        try {
+            connection = SQLDataSource.getInstance().getSQLConnection();
+            connection.setAutoCommit(false);
+             PreparedStatement stmt = connection.prepareStatement("insert into course_section (course_id, semester_id, section_name, total_capacity) values(?, ?, ?, ?);",PreparedStatement.RETURN_GENERATED_KEYS);
+            {
+                int back;
+                stmt.setString(1, courseId);
+                stmt.setInt(2, semesterId);
+                stmt.setString(3, sectionName);
+                stmt.setInt(4, totalCapacity);
+                stmt.executeUpdate();
+                ResultSet resultSet = stmt.getGeneratedKeys();
+                if (resultSet.next()) {
+                    back = resultSet.getInt(3);
+                    connection.commit();
+                    connection.close();
+                    return back;
+                } else {
+                    connection.commit();
+                    connection.close();
+                    throw new IntegrityViolationException();
+                }
             }
-            else {
-                connection.commit();
-                connection.close();
-                //throw exception
+        } catch (SQLException e) {
+            try{
+                if(connection !=null){
+                    connection.rollback();
+                    connection.close();
+                }
+            }catch (SQLException ex){
+                ex.printStackTrace();
             }
 
-        } catch (SQLException e) {
             e.printStackTrace();
+            throw new IntegrityViolationException();
         }
-        return -1;//avoid error
     }
 
     @Override
     public int addCourseSectionClass(int sectionId, int instructorId, DayOfWeek dayOfWeek, Set<Short> weekList, short classStart, short classEnd, String location) {
-        String sql="insert into classes" + "(sectionid, instructorid, dayofweek, week, classbegin, classend, location, classid) " + "values (?,?,?,?,?,?,?,currval('coursesectionclass_classid_seq'));";
         try (Connection connection = SQLDataSource.getInstance().getSQLConnection();
-             PreparedStatement stmt = connection.prepareStatement("insert into classes (section_id, instructor_id, day_of_week, class_begin, class_end, location) values (?,?,?,?,?)")) {
+             PreparedStatement stmt = connection.prepareStatement("insert into classes (section_id, instructor_id, day_of_week, week_list, class_begin, class_end, location) values (?,?,?,?,?,?,?)")) {
             stmt.setInt(1, sectionId);
             stmt.setInt(2, instructorId);
             stmt.setInt(3,dayOfWeek.getValue());
@@ -84,22 +99,77 @@ public class MyCourseService implements CourseService {
 
     @Override
     public void removeCourse(String courseId) {
+        try (Connection connection = SQLDataSource.getInstance().getSQLConnection();
+            PreparedStatement stmt = connection.prepareStatement(
+                    "delete from course where course_id = ?")) {
+            stmt.setString(1,courseId);
 
+            int result = stmt.executeUpdate();
+            if(result<=0) throw new EntityNotFoundException();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void removeCourseSection(int sectionId) {
+        try (Connection connection = SQLDataSource.getInstance().getSQLConnection();
+             PreparedStatement stmt = connection.prepareStatement(
+                     "delete from course_section where section_id = ?")) {
+            stmt.setInt(1,sectionId);
 
+            int result = stmt.executeUpdate();
+            if(result<=0) throw new EntityNotFoundException();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void removeCourseSectionClass(int classId) {
+        try (Connection connection = SQLDataSource.getInstance().getSQLConnection();
+             PreparedStatement stmt = connection.prepareStatement(
+                     "delete from classes where class_id = ?")) {
+            stmt.setInt(1,classId);
 
+            int result = stmt.executeUpdate();
+            if(result<=0) throw new EntityNotFoundException();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public List<Course> getAllCourses() {
-        return null;
+        List<Course> courses = new ArrayList<>();
+        try(Connection conn = SQLDataSource.getInstance().getSQLConnection()){
+            PreparedStatement p = conn.prepareStatement("select * from course");
+            ResultSet resultSet = p.executeQuery();
+
+            while (resultSet.next()){
+                String id = resultSet.getString(1);
+                String name = resultSet.getString(2);
+                int credit = resultSet.getInt(3);
+                int classHour = resultSet.getInt(4);
+                boolean isPF = resultSet.getBoolean(5);
+
+                Course.CourseGrading grading=isPF? Course.CourseGrading.PASS_OR_FAIL:
+                        Course.CourseGrading.HUNDRED_MARK_SCORE;
+
+                Course c=new Course();
+                c.id = id;
+                c.name = name;
+                c.credit = credit;
+                c.classHour = classHour;
+                c.grading = grading;
+
+                courses.add(c);
+            }
+            return courses;
+        }catch (SQLException e){
+            e.printStackTrace();
+            return courses;
+        }
     }
 
     @Override
