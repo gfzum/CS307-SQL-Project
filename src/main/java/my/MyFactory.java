afe14e6093f381519780728608fc2b4580c9681b
jr <1975863521@qq.com>
2021-12-18 00:52:31 +0800

    MyFactory注册

--- src/main/java/my/MyFactory.java
+++ src/main/java/my/MyFactory.java
@@ -1,16 +1,20 @@
 package my;
 
 import cn.edu.sustech.cs307.factory.ServiceFactory;
-import cn.edu.sustech.cs307.service.CourseService;
-import my.services.MyCourseService;
+import cn.edu.sustech.cs307.service.*;
+import my.services.*;
 
 import java.util.List;
 
 public class MyFactory extends ServiceFactory {
     public MyFactory() {
-        super();
         registerService(CourseService.class, new MyCourseService());
-        //hhlkj
+        registerService(DepartmentService.class, new MyDepartmentService());
+        registerService(InstructorService.class, new MyInstructorService());
+        registerService(MajorService.class, new MyMajorService());
+        registerService(SemesterService.class, new MySemesterService());
+        registerService(StudentService.class, new MyStudentService());
+        registerService(UserService.class, new MyUserService());
     }
 
     @Override
